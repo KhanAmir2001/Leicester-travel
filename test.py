d90def431bc0a3b78a815406d79159f50a5a30c2
KhanAmir2001 <KhanAmir2001@Outlook.com>
2022-09-15 00:11:19 +0100

    laid out plan go to sleep

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
-print("hello world")
+#Should output all prices of current travel to leicester at minumum
+#Could live analyse and update
+#Could display line graphs comparing
+
+def train():
+    return()
+
+def bus():
+    return()
+
+def coach():
+    return()
+    
